005eea55f57356c6f0ba0058944ba7c0d969a797
Zachary Sunberg <sunbergzach@gmail.com>
2017-03-29 22:42:40 -0700

    added action_gen

---
+++ src/action_gen.jl
@@ -0,0 +1,11 @@
+immutable GPSFirst
+    rng::MersenneTwister
+end
+
+function POMCP.next_action(gen::GPSFirst, pomdp::PowseekerPOMDP, b, h)
+    if n_children(h) == 0
+        return GPSOrAngle(true, 0.0)
+    else
+        return GPSOrAngle(false, 2*pi*rand(gen.rng))
+    end
+end
