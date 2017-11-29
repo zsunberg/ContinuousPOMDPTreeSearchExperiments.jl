0ededc869edb1fdb29b26683eff319cead71476c
Zachary Sunberg <sunbergzach@gmail.com>
2017-11-29 12:59:01 -0800

    before pull

--- lasertag/pow_opt.jl
+++ lasertag/pow_opt.jl
@@ -13,8 +13,8 @@ using DiscreteValueIteration
 
 function gen_sims(x::Vector{Float64}, n, k)
     c = max(0.0, x[1])
-    k_obs = max(1.0, x[4])
-    inv_alpha_obs = max(0.1, x[5])
+    k_obs = max(1.0, x[2])
+    inv_alpha_obs = max(0.1, x[3])
 
     rng = MersenneTwister(0)
     ro = ValueIterationSolver()
@@ -36,7 +36,7 @@ function gen_sims(x::Vector{Float64}, n, k)
 
     sims = []
     for i in 1:n
-        pomdp = gen_lasertag(MersenneTwister(i+70_000*k))
+        pomdp = gen_lasertag(rng=MersenneTwister(i+70_000*k))
         planner = solve(solver, pomdp)
         filter = ObsAdaptiveParticleFilter(deepcopy(pomdp),
                                            LowVarianceResampler(10_000),
@@ -72,31 +72,34 @@ function run_parallel(sims::Vector{Any})
 end
 =#
 
-# start_mean = [40.0, 8.0, 20.0, 4.0, 20.0]
 start_mean = [20.0, 4.0, 10.0]
 start_cov = diagm([40.0^2, 10.0^2, 10.0^2])
 d = MvNormal(start_mean, start_cov)
 rng = MersenneTwister(15)
-K = 200
-m = 50
+K = 80  # 200
+n = 40  # 40
+m = 20  # 50
+max_iters = 2
 
-for i in 1:100
+for i in 1:max_iters
     sims = []
     params = Vector{Vector{Float64}}(K)
     for k in 1:K
         p = rand(d)
         params[k] = p
-        k_sims = gen_sims(p, 40, k)
+        k_sims = gen_sims(p, n, k)
+        println("appending $(length(k_sims)) simulations")
         append!(sims, k_sims)
     end
     results = run_parallel(sims)
+    # results = run(sims)
     combined = by(results, :k) do df
         DataFrame(mean_reward=mean(df[:reward]))
     end
     @show mean(combined[:mean_reward])
     order = sortperm(combined[:mean_reward])
     elite = params[combined[:k][order[K-m:end]]]
-    elite_matrix = Matrix{Float64}(5, m)
+    elite_matrix = Matrix{Float64}(length(start_mean), m)
     for k in 1:m
         elite_matrix[:,k] = elite[k]
     end
